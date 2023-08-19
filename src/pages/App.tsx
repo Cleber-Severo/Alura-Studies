be2181e77d6126f212adea0b0c5abc7a16df7ded
Cléber Severo <clebersevero1998@gmail.com>
2023-08-19 08:40:37 -0300

    chore: all styles changed to css modules rules

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 
-import style from './style.module.scss'
+import style from './App.module.scss'
 
 function App() {
   return (
