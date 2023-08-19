106f5e4ee2949fac19230648714eb4135b7fbd6b
Cléber Severo <clebersevero1998@gmail.com>
2023-08-19 09:42:46 -0300

    chore: list item key

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,14 +1,13 @@
 import style from './Item.module.scss'
 
 interface ItemProps {
-    index: number
     tarefa: string
     tempo: string
 }
 
-const Item = ( {index, tarefa, tempo}:ItemProps ) => {
+const Item = ( { tarefa, tempo}:ItemProps ) => {
   return (
-    <li key={index} className={style.item}>
+    <li className={style.item}>
       <h3> {tarefa} </h3>
       <span> {tempo} </span>
     </li>
