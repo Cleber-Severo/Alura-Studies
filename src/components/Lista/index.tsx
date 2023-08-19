60826178bf8a9da1f696fb5894af0d49ba5a6988
Cléber Severo <clebersevero1998@gmail.com>
2023-08-18 22:43:22 -0300

    doc: sass files and styles

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './style.scss'
 
 function Lista() {
     const tarefas = [
@@ -17,18 +18,18 @@ function Lista() {
     ]
 
     return (
-        <aside>
-            <h2>Estudos do dia</h2>
-            <ul>
-                { tarefas.map( (item, index) => (
-                    <li key={index}>
-                        <h3> {item.tarefa} </h3>
-                        <span> {item.tempo} </span>
-                    </li>
-                ) ) }
-            </ul>
-        </aside>
-    )
+      <aside className="listaTarefas">
+        <h2>Estudos do dia</h2>
+        <ul>
+          {tarefas.map((item, index) => (
+            <li key={index}>
+              <h3> {item.tarefa} </h3>
+              <span> {item.tempo} </span>
+            </li>
+          ))}
+        </ul>
+      </aside>
+    );
 }
 
 export default Lista;
