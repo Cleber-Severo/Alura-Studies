c659e9633f254a4332a1cb1bdc80e7fbe910129f
Cléber Severo <clebersevero1998@gmail.com>
2023-08-28 11:05:29 -0300

    feat: adding new item via form component dynamically

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,15 +3,30 @@ import Botao from "../Botao";
 import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  };
+
+  adicionarTarefa(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(this.state);
+    
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.tarefa}
+            onChange={(e) =>
+              this.setState({ ...this.state, tarefa: e.target.value })
+            }
             placeholder="O que você quer estudar ?"
             required
           />
@@ -22,15 +37,17 @@ class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={(e) =>
+              this.setState({ ...this.state, tempo: e.target.value })
+            }
             id="tempo"
             min="00:00:00"
             max="01:30:00"
             required
           />
         </div>
-        <Botao>
-          Adicionar
-        </Botao>
+        <Botao>Adicionar</Botao>
       </form>
     );
   }
