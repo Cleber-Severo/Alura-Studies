641f889fef93565cdde016ed26d812451576a34c
Cléber Severo <clebersevero1998@gmail.com>
2023-08-31 21:22:47 -0300

    feat: time state and selected prop typed on clock

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,14 +1,24 @@
 import style from "./Cronometro.module.scss";
 import Botao from "../Botao";
 import Relogio from "./Relogio";
-import { tempoParaSegundos } from "../../common/utils/date";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { Itarefa } from "../../types/tarefa";
+import { useState } from "react";
 
-const Cronometro = () => {
-  console.log('Conversão: ', tempoParaSegundos('01:01:01'));
+interface CronometroProps {
+  selecionado: Itarefa | undefined
+}
+
+const Cronometro = ({selecionado}:CronometroProps) => {
+  const [tempo, setTempo] = useState<number>();
+  if (selecionado?.tempo) {
+    setTempo(tempoParaSegundos(selecionado.tempo));
+  }
   
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: { tempo }
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
